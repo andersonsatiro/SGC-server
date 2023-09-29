91c6420e9588802c64b1bdd863d358d6291acd23
Your Name <you@example.com>
2023-09-29 13:06:34 -0300

    feat: create tables in the DataBase

---
+++ prisma/migrations/20230926234215_create_leader_table/migration.sql
@@ -0,0 +1,8 @@
+-- CreateTable
+CREATE TABLE "Leader" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "numberOfCollaborators" INTEGER NOT NULL,
+
+    CONSTRAINT "Leader_pkey" PRIMARY KEY ("id")
+);
