6e04aba5d0d2a83ba101a6c0cd0c5ce26337d099
Your Name <you@example.com>
2023-09-29 21:01:36 -0300

    feat: delete migrates

--- prisma/migrations/20230926234215_create_leader_table/migration.sql
+++
@@ -1,8 +0,0 @@
--- CreateTable
-CREATE TABLE "Leader" (
-    "id" TEXT NOT NULL,
-    "name" TEXT NOT NULL,
-    "numberOfCollaborators" INTEGER NOT NULL,
-
-    CONSTRAINT "Leader_pkey" PRIMARY KEY ("id")
-);
