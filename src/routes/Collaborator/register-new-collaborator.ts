9c8813f027ae999c4b31f4d3f6b0155bb22a71c7
Your Name <you@example.com>
2023-09-30 21:56:00 -0300

    feat: change folder files

---
+++ src/routes/Collaborator/register-new-collaborator.ts
@@ -0,0 +1,54 @@
+import { prisma } from "../../lib/prisma"
+import { FastifyInstance } from "fastify";
+import { z } from 'zod'
+
+export async function RegisterNewCollaborator(app: FastifyInstance) {
+    app.post('/register/collaborator', async (req, reply) => {
+        const collaboratorSchema = z.object({
+            name: z.string(),
+            salary: z.number(),
+            jobId: z.string().uuid(),
+            leaderId: z.string().uuid()
+        })
+
+        const { name, salary, jobId, leaderId } = collaboratorSchema.parse(req.body)
+
+        const jobPositions = await prisma.jobPositions.findFirstOrThrow({
+            where: {
+                id: jobId
+            }
+        })
+        
+        const leader = await prisma.leader.findFirstOrThrow({
+            where: {
+                id: leaderId
+            }
+        })
+
+        await prisma.collaborator.create({
+            data: {
+                name,
+                jobRole: jobPositions.name,
+                salary,
+                influence: jobPositions.influence,
+                leaderId,
+            }
+        })
+
+        if(leader) {
+            await prisma.leader.update({
+                where: {
+                    id: leaderId
+                },
+                
+                data: {
+                    monthlyIncome: leader.monthlyIncome + salary,
+                    numberOfCollaborators: leader.numberOfCollaborators + 1
+                }
+            })
+        }
+
+        return reply.status(201).send('Registered Collaborator')
+    })
+}
+  
---
+++ src/routes/Leader/register-new-leader.ts
@@ -0,0 +1,31 @@
+import { FastifyInstance } from "fastify"
+import { prisma } from "../../lib/prisma"
+import { z } from 'zod'
+
+export async function RegisterNewLeader(app: FastifyInstance){
+    app.post('/register/leader', async (req, reply) => {
+
+        const leaderSchema = z.object({
+            name: z.string(),
+            jobId: z.string().uuid(),
+        })
+
+        const { name, jobId } = leaderSchema.parse(req.body)
+
+        const leadershipPositions = await prisma.leadershipPositions.findFirstOrThrow({
+            where: {
+                id: jobId
+            }
+        })
+
+        await prisma.leader.create({
+            data: {
+                name,
+                jobRole: leadershipPositions.name,
+                influence: leadershipPositions.influence
+            }
+        })
+
+        return reply.status(201).send('Registered Leader')
+    })
+}
