d735f12b478bc3dcd3bd8832f9a2239165bac716
Your Name <you@example.com>
2023-10-09 22:23:19 -0300

    feat: remove import

--- src/routes/Leader/get-all-leaders.ts
+++ src/routes/Leader/get-all-leaders.ts
@@ -1,6 +1,5 @@
 import fastify, { FastifyInstance } from "fastify";
 import { prisma } from "../../lib/prisma";
-import { Authenticate } from '../../middleware/Authenticate'
 
 export async function GetAllLeaders(app: FastifyInstance) {
     app.get('/leaders', { preHandler: app.authenticate }, async () => {
