c5d61ce2ce27fc182f248dd55c2edd6f67b2a12a
Your Name <you@example.com>
2023-10-28 17:25:27 -0300

    feat: rename route name

--- src/routes/Leader/sort-leaders-by-influence.ts
+++ src/routes/Leader/sort-leaders-by-influence.ts
@@ -2,7 +2,7 @@ import { FastifyInstance } from "fastify"
 import { prisma } from "../../lib/prisma"
 
 export async function SortLeadersByInfluence(app: FastifyInstance) {
-    app.get('/influence', { preHandler: app.authenticate }, async () => {
+    app.get('/leaders/influence', { preHandler: app.authenticate }, async () => {
         const leaders = await prisma.leader.findMany({
             orderBy: {
                 averageInfluence: 'desc'
